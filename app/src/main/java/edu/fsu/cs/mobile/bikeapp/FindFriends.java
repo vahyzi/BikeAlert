fc10c23a3e0a015e956c3827c435e4d8d4b602d1
Bryan Pozzuoli <vahyzi@gmail.com>
2019-04-24 00:13:23 -0400

    Added Bike Info to Friendslist

--- app/src/main/java/edu/fsu/cs/mobile/bikeapp/FindFriends.java
+++ app/src/main/java/edu/fsu/cs/mobile/bikeapp/FindFriends.java
@@ -1,5 +1,6 @@
 package edu.fsu.cs.mobile.bikeapp;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,11 +14,13 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -25,9 +28,13 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -54,7 +61,30 @@ public class FindFriends extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 Log.d("RidersRef", document.getId() + " => " + document.getData());
-                                Rider rider = new Rider(document.getId());
+                                JSONObject reader = new JSONObject(document.getData());
+                                Bike bike;
+                                GeoPoint location;
+                                Timestamp time;
+                                try {
+                                    JSONObject sys  = reader.getJSONObject("bike");
+                                    String bikeMake = sys.getString("make");
+                                    String bikeModel = sys.getString("model");
+                                    String bikeType = sys.getString("type");
+                                    String bikeColor = sys.getString("color");
+                                    String bikeSize = sys.getString("wheel_size");
+                                    String bikeValve = sys.getString("valve");
+                                    String bikeWidth = sys.getString("tire_width");
+                                    location = document.getGeoPoint("location");
+                                    time = document.getTimestamp("timestamp");
+
+
+                                    bike = new Bike(bikeMake,bikeModel,bikeType,bikeColor,bikeSize,bikeWidth,bikeValve);
+
+                                } catch (JSONException e) {
+                                    throw new RuntimeException(e);
+                                }
+                                Rider rider = new Rider(document.getId(),time,location,bike);
+
                                 ridersAdapter.add(rider);
                             }
                         } else {
--- app/src/main/java/edu/fsu/cs/mobile/bikeapp/RidersAdapter.java
+++ app/src/main/java/edu/fsu/cs/mobile/bikeapp/RidersAdapter.java
@@ -21,7 +21,23 @@ public class RidersAdapter extends ArrayAdapter<Rider> {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.users_display_layout, parent, false);
         }
         TextView email = (TextView) convertView.findViewById(R.id.riderEmail);
+        TextView make = (TextView) convertView.findViewById(R.id.bikeMake);
+        TextView model = (TextView) convertView.findViewById(R.id.bikeModel);
+        TextView type = (TextView) convertView.findViewById(R.id.bikeType);
+        TextView color = (TextView) convertView.findViewById(R.id.bikeColor);
+        TextView size = (TextView) convertView.findViewById(R.id.bikeSize);
+        TextView valve = (TextView) convertView.findViewById(R.id.bikeValve);
+        TextView width = (TextView) convertView.findViewById(R.id.bikeWidth);
+
         email.setText(rider.email);
+        make.setText(rider.getBike().getMake());
+        model.setText(rider.getBike().getModel());
+        type.setText(rider.getBike().getType());
+        color.setText(rider.getBike().getColor());
+        size.setText(rider.getBike().getWheel_size());
+        valve.setText(rider.getBike().getValve());
+        width.setText(rider.getBike().getTire_width());
+
         return convertView;
     }
 }
